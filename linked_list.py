5145b919f58dd0106181623ed1a16ae25bc60f19
Valentin Monar <49161336+MonarValentin@users.noreply.github.com>
2019-04-01 16:33:40 +0100

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -4,13 +4,13 @@ class Node:
         self.next = next
 
 
-# Note, these are methods "A method is a function that is stored as a class attribute"
+
 class LinkedList:
     def __init__(self):
         self.head = None
 
     def add(self, item):
-        self.head = Node(item, self.head)
+        self.head = Node(item, self.head) # adding to the linked list
 
     def remove(self):
         if self.is_empty():
@@ -91,7 +91,7 @@ class LinkedList:
         a = self.remove()
         self.append(a)
 
-        #ptr = ptr.next
+       
 
 
 def detect_loop(lst):
@@ -104,76 +104,5 @@ def detect_loop(lst):
         if ptr == nextptr:
             return True
     return False
-
-
-import sys
-
-
-
-def make_ll(line):
-    items = line.strip().split()
-
-    ll = LinkedList()
-
-    for item in items:
-        ll.add(item)
-
-    return ll
-
-
-def make_long_list():
-    lst = [x for x in range(1, 3000, 10)]
-    ll = LinkedList()
-    for item in lst:
-        ll.add(item)
-
-    return ll
-
-
-def add_head_loop(lst):
-    # Add a loop to this list. Normally a bad thing ... here it is just to test the student program
-    if lst.head != None:  # Need at least one item for a loop
-        if lst.head.next == None:
-            lst.head.next = lst.head
-        else:
-            ptr = lst.head
-            while ptr.next != None:
-                ptr = ptr.next
-
-            ptr.next = lst.head
-
-
-def main():
-    lists = []
-    for line in sys.stdin:
-        # Create two versions of this LinkedList
-        no_loop = make_ll(line)  # one normal list
-        loop = make_ll(line)  # one with a loop
-        add_head_loop(loop)  # ... need to add the loop
-
-        # Add these two lists
-        lists.append(no_loop)
-        lists.append(loop)
-
-    # Add a couple of long lists to the tests
-    ll = make_long_list()
-    lists.append(ll)
-
-    ll = make_long_list()
-    add_head_loop(ll)
-    lists.append(ll)
-
-    # add an empty list to the mix
-    lists.append(LinkedList())
-
-    # Test the students function against all these lists
-    for lst in lists:
-        print("Loop" if detect_loop(lst) else "Noop", lst)
-
-
-if __name__ == "__main__":
-    main()
-
-
 
 
